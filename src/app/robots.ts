b57b7ed1a3a90c96d966bdaa936d2ad0f13367b8
Shaunfurtado <shuanf1801@gmail.com>
2024-04-25 11:38:08 +0530

    Improved Metadata Description

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://shaunfurtado.is-a.dev/sitemap.xml',
+    host: 'https://shaunfurtado.is-a.dev',
+  };
+}
