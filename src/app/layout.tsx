8ba90e92e9f6d7f4505b55a6cf6fb53858d33268
Shaunfurtado <shuanf1801@gmail.com>
2024-04-25 12:28:08 +0530

    Check Build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navbar } from '../pages/components/nav';
+import { Navbar } from './components/nav';
 
 const inter = Inter({ subsets: ["latin"] });
 
