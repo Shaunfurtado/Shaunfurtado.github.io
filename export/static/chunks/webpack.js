8c3a3054fef19c4d9dabf529c0e030010c2eedf7
Shaunfurtado <shuanf1801@gmail.com>
2024-04-28 23:32:21 +0530

    FIx

--- export/static/chunks/webpack.js
+++ export/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7d762ee33d8309be"; }
+/******/ 		__webpack_require__.h = function() { return "a3743f84b792d924"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
