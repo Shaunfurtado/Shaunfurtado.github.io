1da95b0b23f3c216078f6b03b3937d5811fa8709
Shaunfurtado <shuanf1801@gmail.com>
2024-05-01 14:58:39 +0530

    Small Change

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -11,21 +11,6 @@ export default function Page() {
         <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
         <div className="prose prose-neutral dark:prose-invert">
           <ThingsIveBuilt />
-        <ul className="list-disc pl-6 p-4">
-          <li>Twitter crypto bot</li>
-          <li>This personal portfolio</li>
-          <li>Code blogs for code snippets and notes management</li>
-          <li>Personal blogging site with hugo</li>
-          <li>Flowchain blockchain supply chain management</li>
-          <li>hotel management system</li>
-          <li>Kickstartup: Crowdfunding With blogs</li>
-          <li>Java GUI text editor</li>
-        </ul>
-        {/* <p>Other projects :</p>
-        <ul>
-          <li>arduino fingerprint door unlock system</li>
-          <li>nodemcu based water tank level monitoring system</li>
-        </ul> */}
         </div>
         <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
         <div className="flex flex-row justify-between items-center mt-0">
