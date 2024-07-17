ec01e585ab2cc0217c68026831e427fa5a020327
Shaunfurtado <shaunf1801@gmail.com>
2024-07-17 13:43:33 +0530

    Update work experience details and technologies used

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -20,18 +20,16 @@ export default function Page() {
   Blockchain Intern at Karpine, Jan 2024 — Mar 2024 (Remote)
 </p>
 <p className="text-neutral-600 dark:text-neutral-400 text-sm">
-  <b>Technologies used:</b> Node.js, C#, Blazor, Nethereum, Git
+  <b>Technologies used:</b> Node.js, React.js, Express.js, Moralis, Git
 </p>
 <p className="p-2">
-  I joined Karpine early to work on a Twitter Crypto Bot for Ethereum and the presale page for the crypto token launch. I worked on the backend systems and ensured smooth operation.
-</p>
+At Karpine, I contributed to cutting-edge blockchain projects:</p>
 <ul className="list-disc pl-6 p-4">
   <li>
-    In Jan 2024, I started working on the Twitter crypto bot. I used Node.js and Etherscan explorer API to interact with the Ethereum blockchain and Twitter API. This bot was able to monitor transactions and tweet about significant events.
+  Developed a real-time transaction monitoring bot using Node.js and the Etherscan explorer API. The bot tracked Ethereum transactions and provided timely updates on significant events.
   </li>
   <li>
-    In Feb 2024, I was tasked with creating the presale page for the crypto token launch. I used C# and Blazor to build a robust and secure backend system that could handle high traffic and ensure a smooth token presale process.
-  </li>
+  Designed and implemented a secure Token Swap Dapp for crypto token launches. Leveraged MERN stack (MongoDB, Express.js, React.js, Node.js) and Moralis APIs for seamless user experience.  </li>
   <li>
     Throughout my internship, I used Git for version control and collaborated with other team members to ensure the successful completion of the projects.
   </li>
@@ -49,20 +47,20 @@ export default function Page() {
   <b>Technologies used:</b> React.js, HTML, CSS, Javascript, Figma, Git
 </p>
 <p className="p-2">
-  I joined Motioncut, a company well known for its motion graphics and video editing elements. During my internship, I was tasked with four major projects, each with a different focus and set of challenges.
+At Motioncut, I worked on diverse web projects:
 </p>
 <ul className="list-disc pl-6 p-4">
   <li>
-    In the first week, I developed a professional website for business. This involved creating a clean and intuitive UI/UX design, implementing responsive layouts, and ensuring optimal performance.
+    Developed a professional website for business. This involved creating a clean and intuitive UI/UX design, implementing responsive layouts, and ensuring optimal performance.
   </li>
   <li>
-    In the second week, I worked on an e-commerce website. I focused on responsiveness to ensure a seamless shopping experience across different devices. I also integrated a secure payment gateway for transactions.
+    Worked on an e-commerce website. I focused on responsiveness to ensure a seamless shopping experience across different devices. I also integrated a secure payment gateway for transactions.
   </li>
   <li>
-    In the third week, I focused on security measures, particularly form validation. I implemented various validation checks to prevent malicious attacks, ensuring the safety and integrity of user data.
+    Focused on security measures, particularly form validation. I implemented various validation checks to prevent malicious attacks, ensuring the safety and integrity of user data.
   </li>
   <li>
-    In the final week, I created an interactive to-do list application. This involved creating a user-friendly interface, implementing CRUD operations, and integrating a database for persistent storage.
+    Created an interactive to-do list application. This involved creating a user-friendly interface, implementing CRUD operations, and integrating a database for persistent storage.
   </li>
 </ul>
 <p>
